ee97e5a30c136feda5f3c1eea851ad6ce3d43f7f
weaq <oooweaqooo@gmail.com>
2023-03-16 11:54:55 +0700

    next auth google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,49 +1,61 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-    
-    providers: [
-        CredentialsProvider({
-          name: 'member',
 
-          credentials: {
-            username: { label: "Username", type: "text" },
-            password: {  label: "Password", type: "password" }
-          },
-          async authorize(credentials, req) {
-            
-            const res = await fetch("http://localhost:3333/login", {
-              method: 'POST',
-              body: JSON.stringify(credentials),
-              headers: { "Content-Type": "application/json" }
-            })
-            const data = await res.json()
+  providers: [
+    CredentialsProvider({
+      name: 'member',
 
-            if (data.status == 'ok') {
-                return data.user
-            }
-      
-            // Return null if user data could not be retrieved
-            return null
-          }
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+
+        const res = await fetch("http://localhost:3333/login", {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" }
         })
-      ],
-      secret: "LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCx5mLg=",
-      callbacks: {
-        async jwt({ token, user, account }) {
-          // Persist the OAuth access_token to the token right after signin
-          if (account) {
-            token.accessToken = account.access_token
-            token.user = user
-          }
-          return token
-        },
-        async session({ session, token, user }) {
-          // Send properties to the client, like an access_token from a provider.
-          session.accessToken = token.accessToken
-          session.user = token.user
-          return session
+        const data = await res.json()
+
+        if (data.status == 'ok') {
+          return data.user
+        }
+
+        // Return null if user data could not be retrieved
+        return null
+      }
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
         }
       }
-  })
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+        token.user = user
+      }
+      return token
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken
+      session.user = token.user
+      return session
+    }
+  }
+})
